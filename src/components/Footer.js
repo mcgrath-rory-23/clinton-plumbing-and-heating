9ca6a36a53dad0b2dcec3aebcdf948b160cd76a2
mcgrath-rory-23 <mcgrath.r23@gmail.com>
2024-10-19 00:08:09 +0100

    Updated header and footer components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
 function Footer() {
     return (
         <footer>
+            <div className="container">
+                <div className="row">
+                    {/* Contact Information */}
+                    <div className="col-md-6">
+                        <h3>Contact Information</h3>
+                        <ul>
+                            <li>Email: <a href="mailto:john@example.com">john@example.com</a></li>
+                            <li>Phone: <a href="tel:+1234567890">+1234567890</a></li>
+                        </ul>
+                    </div>
+
+                    {/* Social Media Links */}
+                    <div className="col-md-6">
+                        <h3>Follow Me</h3>
+                        <ul>
+                            <li><a href="https://www.facebook.com" target="_blank" rel="noreferrer">Facebook</a></li>
+                            <li><a href="https://www.instagram.com" target="_blank" rel="noreferrer">Instagram</a></li>
+                            {/* Add more social links as needed */}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            {/* Scroll to Top Button */}
+            <button onClick={() => scroll.scrollToTop()} className="gotop">
+                Go to Top
+            </button>
+
+            {/* Copyright Information */}
             <p>© 2024 Clinton Plumbing & Heating. All rights reserved.</p>
         </footer>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'; // Assuming you will add custom CSS here
 
 function Header() {
     return (
-        <header>
+        <header className="header">
+            {/* Background image or video */}
+            <div className="header-background">
+                <video src="./video.mp4" loop autoPlay muted className="header-video"></video>
+                {/* If you prefer an image, you can replace the video tag with this: */}
+                {/* <img src="./path/to/background.jpg" alt="Plumbing background" className="header-image" /> */}
+            </div>
+
+            {/* Main heading */}
             <h1>Clinton Plumbing & Heating</h1>
+
+            {/* Call-to-action buttons */}
+            <div className="header-buttons">
+                <Link to="/contact" className="btn request-quote-btn">Request a Quote</Link>
+                <a href="tel:+1234567890" className="btn contact-btn">Contact Me</a>
+            </div>
         </header>
     );
 }
